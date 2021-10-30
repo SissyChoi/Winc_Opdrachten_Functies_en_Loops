f8769703f52aacc39134857f2ae4e3cf40217cc8
SissyChoi <90140566+SissyChoi@users.noreply.github.com>
2021-10-30 16:46:04 +0200

    Create Opdr_015_Javascript_Function_Basics_Make_Pizza_Script.js

---
+++ Opdr_015_Javascript_Function_Basics_Make_Pizza_Script.js
@@ -0,0 +1,25 @@
+const makePizza = function () {
+    console.log("Mix flower, water and egg");
+    console.log("Kneed the ingredients until you have dough");
+    console.log("Roll out the dough into a circled shape");
+    console.log("Spread tomato sauce on the dough");
+    console.log("Add all your favorite toppings");
+    console.log("Put your pizza in preheated oven");
+};
+
+makePizza();
+makePizza();
+makePizza();
+
+const makeSushi = function () {
+    console.log("Cook and marinade the sushi rice");
+    console.log("While letting the rice cool down, chop all the ingredients");
+    console.log("Add a layer of rice and add toppings");
+    console.log("Roll your sushi tight and cut it into even pieces");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
