3e88d1a60c8a13d9883f0c3f09136c8171f0d840
SissyChoi <90140566+SissyChoi@users.noreply.github.com>
2021-10-31 13:19:09 +0100

    Create Opdr_018_JavaScript_Functions_ThreeWaysofWriting_Script.js

---
+++ Opdr_018_JavaScript_Functions_ThreeWaysofWriting_Script.js
@@ -0,0 +1,29 @@
+// Function declaration
+function multiply1 (number1, number2) {
+    const number1Squared = number1 * number1;
+    const number2Squared = number2 * number2;
+    const sum = number1Squared + number2Squared;
+    const sumSquared = sum * sum;
+    return sumSquared;
+}
+console.log(multiply1(2, 4));
+
+// Function expression
+const multiply2 = function (number1, number2) {
+    const number1Squared = number1 * number1;
+    const number2Squared = number2 * number2;
+    const sum = number1Squared + number2Squared;
+    const sumSquared = sum * sum;
+    return sumSquared;
+};
+console.log(multiply2(2, 4));
+
+//Arrow function
+const multiply3 = (number1, number2) => {
+    const number1Squared = number1 * number1;
+    const number2Squared = number2 * number2;
+    const sum = number1Squared + number2Squared;
+    const sumSquared = sum * sum;
+    return sumSquared;
+};
+console.log(multiply3(2, 4));
