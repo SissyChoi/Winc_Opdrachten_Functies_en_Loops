7b17a077297bfceabc1cec0183e1bf48cd651b6f
SissyChoi <90140566+SissyChoi@users.noreply.github.com>
2021-11-05 17:21:25 +0100

    Create Opdr_020_JavaScript_ForLoops_Mulitplication.js

---
+++ Opdr_020_JavaScript_ForLoops_Mulitplication.js
@@ -0,0 +1,14 @@
+// var multiplier = 9;
+// for (let i = 0; i <= 10; i++) {
+//   let result = multiplier * i;
+//   console.log(multiplier + " * " + i + " = " + result);
+// }
+
+//Bonus excersise
+
+for (let multiplier = 0; multiplier <= 10; multiplier++) {
+  for (let i = 0; i <= 10; i++) {
+    let result = multiplier * i;
+    console.log(multiplier + " * " + i + " = " + result);
+  }
+}
